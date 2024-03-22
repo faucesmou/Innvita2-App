cee76217af34690f553eb49f4614fab71e4d5c00
CrisAndes <gonzalomorresi@gmail.com>
2024-03-22 12:53:17 -0300

    Subiendo incio del proyecto y estructura inicial.

---
+++ src/ComponentsApp.tsx
@@ -0,0 +1,22 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { Text, View } from 'react-native'
+
+
+  export const ComponentsApp = () => {
+    return (
+
+    <NavigationContainer>
+
+    <View>
+      <Text>
+        componente App Inicio
+      </Text>
+    </View>
+
+    </NavigationContainer>    
+  )
+
+}
---
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { AirplaneIcon } from '../../icons/icons'
+
+export const HomeScreen = () => {
+  return (
+    <View>
+        <Text>
+            Home Screen
+        </Text>
+        <AirplaneIcon/>
+        
+    </View>
+  )
+}
