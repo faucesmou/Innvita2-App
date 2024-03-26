f744303246daefafdc7c71a7e2a242a711d18cb6
CrisAndes <gonzalomorresi@gmail.com>
2024-03-26 16:53:38 -0300

    Se incorpora Modal.

--- src/presentation/navigator/Navigator.tsx
+++ src/presentation/navigator/Navigator.tsx
@@ -7,6 +7,7 @@ import { AlertScreen } from '../screens/alerts/AlertScreen';
 import { TextInputScreen } from '../screens/inputs/TextInputScreen';
 import { PullToRefreshScreen } from '../screens/ui/PullToRefreshScreen';
 import { CustomSectionListScreen } from '../screens/ui/CustomSectionListScreen';
+import { ModalScreen } from '../screens/ui/ModalScreen';
 
 const Stack = createStackNavigator();
 
@@ -16,7 +17,7 @@ export const Navigator = () => {
       <Stack.Screen name="HomeScreen" component={ HomeScreen } />
       <Stack.Screen name="Animation101Screen" component={ Animation101Screen } />
       <Stack.Screen name="Animation102Screen" component={ Animation102Screen } />
-      
+
       <Stack.Screen name="SwitchScreen" component={ SwitchScreen } />
       <Stack.Screen name="AlertScreen" component={ AlertScreen } />
 
@@ -24,6 +25,7 @@ export const Navigator = () => {
       <Stack.Screen name="PullToRefreshScreen" component={ PullToRefreshScreen } />
 
       <Stack.Screen name="CustomSectionListScreen" component={ CustomSectionListScreen } />
+      <Stack.Screen name="ModalScreen" component={ ModalScreen } />
 
     </Stack.Navigator>
   );
---
+++ src/presentation/screens/ui/ModalScreen.tsx
@@ -0,0 +1,56 @@
+import { Modal, Platform, Text, View } from 'react-native'
+import { CustomView } from '../../components/ui/CustomView'
+import { Title } from '../../components/ui/Title'
+import { Button } from '../../components/Button'
+import { useState } from 'react'
+
+
+export const ModalScreen = () => {
+
+    const [isVisible, setisVisible] = useState(false);
+
+    return (
+        <CustomView margin >
+
+            <Title text='Modal' safe />
+
+
+            <Button
+                text='Abrir Modal'
+                onPress={() => setisVisible(true)}
+            />
+
+            <Modal
+                visible={isVisible}
+                animationType='slide'/* también esta 'slide' */
+            >
+
+                <View style={{
+                    flex: 1,
+                    backgroundColor: 'rgba(0,0,0,0.1)'
+                        }} >
+                    <View style={{ paddingHorizontal: 10 }} >
+                        <Title text='Modal content' safe />
+                    </View>
+                    <View style={{ flex: 1 }} />
+
+                <Button 
+                text='cerrar modal perro'
+                onPress={ () => setisVisible(false) }
+                styles={{ height: Platform.OS === 'android' ? 40 : 60,
+                borderRadius: 0 }}
+                />
+
+            {/*     <Button 
+                text='cerrar modal 2 perro'
+                onPress={ () => setisVisible(false) }
+                styles={{ paddingHorizontal: 10, marginTop: 40 }}
+                /> */}
+
+                </View>
+
+            </Modal>
+
+        </CustomView>
+    )
+}
